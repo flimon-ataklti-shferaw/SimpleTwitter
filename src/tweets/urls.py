b7f992b8dcb1357600e4262b1322a4f41359921d
teliyadu <flimon.shferaw@gmail.com>
2021-12-16 20:08:26 +0400

    tweets- function based views changed to class based views

--- src/tweets/urls.py
+++ src/tweets/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
+from django.conf.urls import url
 
-
-from .views import list, detail, create, delete, update
+from .views import TweetListView, TweetDetailView, create, delete, update
 
 urlpatterns = [
-    path(r'', list, name='list'),
-    path(r'detail/', detail, name='detail'),
+    path(r'', TweetListView.as_view(), name='list'),
+    url(r'^(?P<pk>\d+)/$', TweetDetailView.as_view(), name='detail'),  # /tweet/1/
     path(r'create/', create, name='create'),
     path(r'delete/', delete, name='delete'),
     path(r'update/', update, name='update'),
--- src/tweets/views.py
+++ src/tweets/views.py
@@ -1,14 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import DetailView, ListView
 
-def list(request):
-    context = {
-    }
-    return render(request, "tweets/list.html", context)
+from .models import Tweet
 
-def detail(request):
+
+class TweetDetailView(DetailView):
+    queryset = Tweet.objects.all()
+
+
+class TweetListView(ListView):
+    template_name = "tweets/list.html"
+    queryset = Tweet.objects.all()
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(TweetListView, self).get_context_data(*args, **kwargs)
+        return context
+
+
+def tweet_detail_view(request, pk=None):  # pk == id
+    obj = get_object_or_404(Tweet, pk=pk)
+    print(obj)
     context = {
+        "object": obj
     }
     return render(request, "tweets/detail.html", context)
+
 
 def create(request):
     context = {
