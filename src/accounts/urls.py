a39744f2fee611f7edbd7c89776f0ed3eb5ec9a7
teliyadu <flimon.shferaw@gmail.com>
2021-12-17 01:13:55 +0400

    accounts-follow toggle

--- src/accounts/urls.py
+++ src/accounts/urls.py
@@ -3,10 +3,12 @@ from django.conf.urls import url
 from django.views.generic.base import RedirectView
 
 from .views import (
-    UserDetailView
+    UserDetailView,
+    UserFollowView
 )
 
 app_name = 'accounts'
 urlpatterns = [
     url(r'^(?P<username>[\w.@+-]+)/$', UserDetailView.as_view(), name='detail'),  # /tweet/1/
+    url(r'^(?P<username>[\w.@+-]+)/follow/$', UserFollowView.as_view(), name='follow'),
 ]
